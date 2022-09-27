bb8adffbcf285dcb551addc6fada30df82d2360e
jgandrade1 <johnglenn.andrade@gmail.com>
2022-09-27 11:36:09 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import SideLinks from "./components/SideLinks";
+
+function App() {
+  return (
+    <div className="App">
+      <SideLinks />
+      <Nav />
+      <Hero />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/nav.css';
+
+function Nav() {
+    return (
+        <nav>
+            <h1>jg.</h1>
+            <ul>
+                <li>About</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
