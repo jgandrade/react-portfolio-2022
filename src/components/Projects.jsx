47005eb00331c038647ddc2ac3c60303a9a47c7b
jgandrade1 <johnglenn.andrade@gmail.com>
2022-11-06 03:11:21 +0800

    Updated Links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
                 <div className='proj-desc' data-aos="fade-up">
                     <h3>E-Commerce</h3>
                     <p>A full stack ecommerce web app created using the MERN Stack</p>
-                    <button>View Live</button>
+                    <a href='https://ecommerce-app-jgandrade.vercel.app/' target="_blank"><button>View Live</button></a>
                     <button>View Code</button>
                 </div>
             </div>
@@ -23,7 +23,7 @@ function Projects() {
                 <div className='proj-desc' data-aos="fade-up">
                     <h3>E-Commerce Backend</h3>
                     <p>A Backend for the E-Commerce App. Where NodeJS, ExpressJS and MongoDB was used</p>
-                    <button>View Code</button>
+                    <a href='https://github.com/jgandrade/ecommerce-api-revised' target="_blank"><button>View Code</button></a>
                 </div>
             </div>
             <div className='project'>
@@ -33,8 +33,19 @@ function Projects() {
                 <div className='proj-desc' data-aos="fade-up">
                     <h3>Developer Portfolio</h3>
                     <p>A portfolio made as a capstone for the bootcamp I came from</p>
-                    <button>View Live</button>
-                    <button>View Code</button>
+                    <a href="https://jgandrade.github.io/my-portfolio/" target="_blank"><button>View Live</button></a>
+                    <a href="https://github.com/jgandrade/my-portfolio" target="_blank"><button>View Code</button></a>
+                </div>
+            </div>
+            <div className='project'>
+                <div className='image' data-aos="fade-up" data-aos-delay="300">
+                    <a href="https://todolist-app-omega.vercel.app/" target="_blank"><img srcSet='./todolist3.PNG' /></a>
+                </div>
+                <div className='proj-desc' data-aos="fade-up">
+                    <h3>Todolist v3</h3>
+                    <p>A todo list app I improved after getting in touch with React</p>
+                    <a href="https://todolist-app-omega.vercel.app/" target="_blank"><button>View Live</button></a>
+                    <a href="https://github.com/jgandrade/todolist-app-v3" target="_blank"><button>View Code</button></a>
                 </div>
             </div>
         </div>
